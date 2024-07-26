61e7e7dcda2f4a7c8ca20f308319b151cf0633cf
Lonz8576 <Lonz8576@users.noreply.github.com>
2024-07-26 13:47:32 -0400

    logo added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-
+import logo from "./dictionary-logo-transparent.png";
 import './App.css';
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
+      <header className='App-header text-center'>
+        <img src={logo} className='logo' alt='logo' width={200}/>
+      </header>
       Dictionary
     </div>
   );
 }
 
-export default App;
+ 
