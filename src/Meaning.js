d6711d25d0bd3bdd6c661be4bf0e23a6ca2f47a2
Lonz8576 <Lonz8576@users.noreply.github.com>
2024-07-29 14:48:10 -0400

    synonyms component
    styling for EVERYTHING needed

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,21 +1,30 @@
 import React from "react";
-
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
     return (
       <div className="meanings">
         <div className="partOfSpeech">
             {props.meaning.partOfSpeech}
+            <br />
         </div>
         
             {props.meaning.definitions.map(function(definition, index) {
                 return (
                   <div key={index} className="definition">
+                     <span className="title"> Definition: </span>
+                    <br />
                      {definition.definition}
-                    
+                     <br />
                    <div className="example">
+                   <br />
+                   <span className="title"> Example: </span>
+                    <br />
                     {definition.example}
                    </div>
+                   <br />
+                 <Synonyms synonyms={definition.synonyms} />
+                <hr></hr>
                    </div>
            
                 );
---
+++ src/Synonyms.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Synonyms(props) {
+    if (props.synonyms) {
+         return (
+            <ul className="Synonyms">
+                <div className="synTitle">Synonyms:</div>
+              {props.synonyms.map(function(synonym, index) {
+                return ( 
+                    
+                    <li key={index}> {synonym} </li>
+                        
+                )      
+                })}
+                </ul>
+        );
+    } else {
+         return null;
+    }
+   }
+
