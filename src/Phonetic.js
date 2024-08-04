545d1e69694f863460cdbeb8b9ff056bce4b0ee9
Lonz8576 <Lonz8576@users.noreply.github.com>
2024-08-04 15:58:32 -0400

    touch-ups styling

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,17 +2,16 @@ import React from "react";
 
 export default function Phonetic(props) {
     return (
-        <div className="Phonetic">
-            <div className="d-flex justify-content-end">
-            <div className="phonetic-word col-2">
+        <div className="Phonetic justify-content-center">
+            <div className="d-flex row justify-content-end">
+            <div className="phonetic-word col-3">
               {props.phonetic.text}
-              </div>
-             
-            <div className="col-2">
-             <div className="hear"><a href={props.phonetic.audio} target="_blank"><i class="bi bi-music-note-list listen"></i>  
-            </a></div> 
+              <div className="hear-row row py-1">
+             <div className="hear justify-content-end"><a href={props.phonetic.audio} target="_blank"><i class="bi bi-music-note-list listen align-middle"></i>  
+            </a>
             </div>
-
+            </div>
+            </div>
             </div>
         </div>
     );
